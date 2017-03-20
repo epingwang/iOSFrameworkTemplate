7031522dac79b79a158d3580786c9fece6d61e29
王易平 <me@epingwang.me>
2017-03-20 14:57:40 +0800

    change comments

--- {{projectname}}Demo/{{projectname}}Demo/AppDelegate.h
+++ {{projectname}}Demo/{{projectname}}Demo/AppDelegate.h
@@ -1,9 +1,9 @@
 //
 //  AppDelegate.h
-//  FrameworkDemo
+//  {{projectname}}Demo
 //
-//  Created by 王易平 on 2017/3/17.
-//  Copyright © 2017年 LeTV Public Components Group. All rights reserved.
+//  Created by {{author}} on {{date}}.
+//  Copyright © 2017年 {{organizationname}}. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
